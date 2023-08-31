a6664ac3ec7fec796c7f4f160b10b801d2bda7ae
Leandro Campagnaro <lcampagnaro@gmail.com>
2023-08-31 11:26:32 -0300

    2

--- Antecessor e Sucessor em PHP/resposta.php
+++ Antecessor e Sucessor em PHP/resposta.php
@@ -3,25 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
+    <title>Desafio Antecessor e Sucessor</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        <h1>Resultados do processamento</h1>
-    </header>
     <main>
-        <?php 
-            //var_dump($_REQUEST);
-            $nome = $_REQUEST["nome"] ?? "nome";
-            $sobrenome = $_REQUEST["sobrenome"] ?? "sobrenome";
-            $idade = $_REQUEST["idade"] ?? "??";
-            echo "<p>É um prazer te conhecer, <strong>$nome $sobrenome</strong>. Vi que você tem $idade anos!</p>";
-        ?>
-        <br>
+        <h1>Resultado Final</h1>
+        <p>
+            <?php 
+                $n = $_REQUEST["num"] ?? 0;
+                $a = $n - 1;
+                $s = $n + 1;
+                echo "O número escolhido foi <strong>$n</strong>!";
+                echo "<br>O seu <em>antecessor</em> é <strong>$a</strong>!";
+                echo "<br>O seu <em>sucessor</em> é <strong>$s</strong>!";
+            ?>
+        </p>
         <button onclick="javascript:window.location.href='index.html'">&#x2B05; Voltar</button> <!--Nesse, o javascript é quem chama o "arquivo" de index, como página em branco-->
 
         <!--<button onclick="javascript:histoy.go(-1)">&#x2B05; Voltar</button>-->  <!--Nesse caso, usa o histórico para voltar para a página anterior. O Arquivo permanece com as informações preenchidas anteriormente-->
-    </main>    
+    </main>
+    
 </body>
 </html>
